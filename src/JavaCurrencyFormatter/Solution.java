3f62fc186b25895d43ac53fd4e243670fa54af08
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-11-03 15:18:30 +0900

    Initial commit

---
+++ src/JavaCurrencyFormatter/Solution.java
@@ -0,0 +1,28 @@
+package JavaCurrencyFormatter;
+
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        Locale indiaLocale = new Locale("en", "IN");
+        NumberFormat format = NumberFormat.getCurrencyInstance(indiaLocale);
+        String india = format.format(payment);
+
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
